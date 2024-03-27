dee4bb382bda8af9fa19ea34166e245eb5917109
Shahid Raza <shahidraza02qwert@gmail.com>
2024-03-28 01:12:49 +0530

    modify

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,14 +1,12 @@
-import { Box, FormControlLabel, FormGroup, Switch} from '@mui/material'
+import { Box, Switch} from '@mui/material'
 import React from 'react'
 import Post from './Post'
 
 function Feed({mode  , setMode}) {
-  
+ 
   return (
     <Box  flex={4} p={2}>
-      <FormGroup >
-      <FormControlLabel control={<Switch />} label="Dark"onClick={()=>setMode(mode==="light" ? 'dark':'light')} />
-      </FormGroup>
+      <Switch  onChange={(e)=>setMode(mode==="light" ? 'dark':'light')} sx={{display:{sx:'block',md:'none'}}} />
       <Post/>
       <Post/>
       <Post/>
