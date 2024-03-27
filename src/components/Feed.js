629fa58c8233ccfddceb78eb2feeb61787157b6b
Shahid Raza <shahidraza02qwert@gmail.com>
2024-03-28 01:06:20 +0530

    modify file

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,9 @@
-import { Box, FormControlLabel, FormGroup, Switch, styled} from '@mui/material'
+import { Box, FormControlLabel, FormGroup, Switch} from '@mui/material'
 import React from 'react'
 import Post from './Post'
 
 function Feed({mode  , setMode}) {
-  const CustomBox=styled('div')({
-     display:'flex',
-     justifyContent:"flex-end"
-  })
+  
   return (
     <Box  flex={4} p={2}>
       <FormGroup >
